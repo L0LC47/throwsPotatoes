b7ff0ea5f119bc7d4b905aa65b3bf4b0750e5a7f
ruffo92 <alberto.miglio92@gmail.com>
2015-04-23 17:18:36 +0200

    Added things
    added absyn/Fixture.java
    added type/FixtureDeclaration.java

---
+++ src/absyn/FixtureDeclaration.java
@@ -0,0 +1,137 @@
+package absyn;
+
+import java.io.FileWriter;
+
+import semantical.TypeChecker;
+import types.ClassType;
+import types.CodeSignature;
+import types.FixtureSignature;
+import types.TypeList;
+import types.VoidType;
+
+/**
+ * A node of abstract syntax representing the declaration of a constructor
+ * of a Kitten class.
+ *
+ * @author <A HREF="mailto:info@l0lc47.tk">L0LC47</A>
+ */
+
+public class FixtureDeclaration extends ClassMemberDeclaration {
+
+    /**
+     * The signature of this fixture or method. This is {@code null} if this
+     * fixture or method has not been type-checked yet.
+     */
+
+    private CodeSignature sig;
+    
+	private Command body;
+
+	/**
+	 * Constructs the abstract syntax of a fixture declaration.
+	 *
+	 * @param pos the starting position in the source file of
+	 *            the concrete syntax represented by this abstract syntax
+	 * @param body the abstract syntax of the body of the fixture
+	 * @param next the abstract syntax of the declaration of the
+	 *             subsequent class member, if any
+	 */
+
+	public FixtureDeclaration(int pos, Command body, ClassMemberDeclaration next) {
+		super(pos,next);
+    	this.body = body;
+	}
+	
+	/**
+     * Specifies the code signature of this fixture.
+     *
+     * @param sig the code signature of this fixture.
+     */
+
+    protected void setSignature(CodeSignature sig) {
+    	this.sig = sig;
+    }
+
+    /**
+     * Yields the signature of this method or fixture declaration.
+     *
+     * @return the signature of this method or fixture declaration.
+     *         Yields {@code null} if type-checking has not been performed yet
+     */
+
+    @Override
+    public CodeSignature getSignature() {
+    	return sig;
+    }
+    
+    /**
+    * Yields the abstract syntax of the body of the constructor or method.
+    *
+    * @return the abstract syntax of the body of the constructor or method
+    */
+
+    public Command getBody() {
+    	return body;
+    }
+
+	/**
+	 * Adds arcs between the dot node for this piece of abstract syntax
+	 * and those representing the formal parameters and body of the fixture.
+	 *
+	 * @param where the file where the dot representation must be written
+	 */
+
+	protected void toDotAux(FileWriter where) throws java.io.IOException {
+		linkToNode("body", getBody().toDot(where), where);
+	}
+	
+	/**
+	 * Adds the signature of this fixture declaration to the given class.
+	 *
+	 * @param clazz the class where the signature of this fixture
+	 *              declaration must be added
+	 */
+
+	@Override 
+	protected void addTo(ClassType clazz) {
+		FixtureSignature cSig = new FixtureSignature (clazz, this);
+		// TODO:
+		//clazz.addFixture(cSig);
+
+		// we record the signature of this constructor inside this abstract syntax
+		// TODO:
+		// setSignature(cSig);
+	}
+
+	/**
+	 * Type-checks this fixture declaration. Namely, it builds a type-checker
+	 * whose only variable in scope is {@code this} of the defining class of the
+	 * fixture, and where only return instructions of type {@code void} are allowed.
+	 * It then type-checks the body of the fixture in that type-checker
+	 * and checks that it does not contain any dead-code.
+	 *
+	 * @param clazz the semantical type of the class where this constructor occurs.
+	 */
+
+	@Override
+	protected void typeCheckAux(ClassType clazz) {
+		
+		TypeChecker checker = new TypeChecker(VoidType.INSTANCE, clazz.getErrorMsg());
+		checker = checker.putVar("this", clazz);
+
+		// we type-check the body of the constructor in the resulting type-checker
+		getBody().typeCheck(checker);
+
+		// we check that there is no dead-code in the body of the constructor
+		getBody().checkForDeadcode();
+
+		// if our superclass exists, it must contain an empty constructor,
+		// that will be chained to this constructor
+		if (clazz.getSuperclass() != null && clazz.getSuperclass().constructorLookup(TypeList.EMPTY) == null)
+			error(checker, clazz.getSuperclass() + " has no empty constructor");
+
+		// constructors return nothing, so that we do not check whether
+		// a return statement is always present at the end of every
+		// syntactical execution path in the body of a constructor
+	}
+}
---
+++ src/types/FixtureSignature.java
@@ -0,0 +1,125 @@
+package types;
+
+import translation.Block;
+import absyn.FixtureDeclaration;
+import bytecode.CONSTRUCTORCALL;
+import bytecode.LOAD;
+
+/**
+ * The signature of a piece of code of a Kitten class.
+ *
+ * @author <A HREF="mailto:info@l0lc47.tk">L0LC47</A>
+ */
+
+public class FixtureSignature extends ClassMemberSignature {
+
+    /**
+     * The intermediate Kitten code for this constructor or method.
+     * This is {@code null} if this constructor or method has not been
+     * translated yet.
+     */
+
+    private Block code;
+
+    /**
+     * Builds a signature for a fixture object.
+     *
+     * @param clazz the class where this code is defined
+     * @param abstractSyntax the abstract syntax of the declaration of this code
+     */
+
+    public FixtureSignature(ClassType clazz, FixtureDeclaration abstractSyntax) {
+
+    	super(clazz,abstractSyntax);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+    		return false;
+    }
+
+    @Override
+    public int hashCode() {
+    	return getDefiningClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+    	return getDefiningClass().toString();
+    }
+
+    /**
+     * Yields the abstract syntax of this fixture declaration.
+     *
+     * @return the abstract syntax of this fixture declaration
+     */
+
+    @Override
+    public FixtureDeclaration getAbstractSyntax() {
+    	return (FixtureDeclaration) super.getAbstractSyntax();
+    }
+
+    /**
+     * Yields the block where the Kitten bytecode of this fixture.
+     *
+     * @return the block where the Kitten bytecode of this fixture.
+     */
+
+    public Block getCode() {
+    	return code;
+    }
+
+    /**
+     * Sets the Kitten code of this fixture, adding
+     * automatically the prefix expected for it.
+     *
+     * @param code the Kitten code
+     */
+
+    public void setCode(Block code) {
+    	this.code = addPrefixToCode(code);
+    }
+
+    /**
+     * Adds a prefix to the Kitten bytecode generated for this fixture. 
+     * This allows for instance constructors to add a call to the
+     * constructor to the superclass.
+     *
+     * @param code the code already compiled for this fixture
+     * @return {@code code} with a prefix
+     */
+
+    // TODO:
+    protected Block addPrefixToCode(Block code){
+		// we prefix a piece of code that calls the constructor of
+		// the superclass (if any)
+		if (!getDefiningClass().getName().equals("Object")) {
+			ClassType superclass = getDefiningClass().getSuperclass();
+
+			code = new LOAD(0, getDefiningClass()).followedBy
+				(new CONSTRUCTORCALL(superclass.constructorLookup(TypeList.EMPTY))
+				.followedBy(code));
+		}
+
+		return code;
+    }
+
+    /**
+     * Generates an invocation instruction that calls this fixture.
+     *
+     * @param classGen the class generator to be used to generate the
+     *                 invocation instruction
+     * @param invocationType the type of invocation required, as enumerated
+     *                       inside {@code org.apache.bcel.Constants}
+     * @return an invocation instruction that calls this fixture.
+     */
+
+    // TODO:
+    /*protected InvokeInstruction createInvokeInstruction(JavaClassGenerator classGen, short invocationType) {
+    	// we use the instruction factory in order to put automatically inside
+    	// the constant pool a reference to the Java signature of this method or constructor
+    	return classGen.getFactory().createInvoke
+   			(getDefiningClass().toBCEL().toString(), // name of the class
+			invocationType); // the type of invocation (static, special, ecc.)
+    }*/
+}
